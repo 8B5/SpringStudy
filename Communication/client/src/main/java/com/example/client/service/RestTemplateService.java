432773825f2838451a93c173f1c1229ce8683ece
8b5 <tlsdpwl2299@naver.com>
2021-08-15 23:11:41 +0900

    제네릭을 사용해 재사용이 가능한 json 형태의 데이터 받기

--- Communication/client/src/main/java/com/example/client/service/RestTemplateService.java
+++ Communication/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,6 +1,12 @@
 package com.example.client.service;
 
+import com.example.client.dto.Req;
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
+import org.apache.catalina.User;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -23,10 +29,120 @@ public class RestTemplateService {
         System.out.println(uri.toString());
         RestTemplate restTemplate = new RestTemplate();
         //String result = restTemplate.getForObject(uri, String.class); //받을 타입 지정
+        //getForEntity : http의 get메소드를 Entity로 가져오겠다.
         ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
         //ResponseEntity를 사용하면 getStatusCode,getBody 사용 가능
+
         System.out.println(result.getStatusCode()); //200 OK
         System.out.println(result.getBody()); //내용 //hello server
+
         return result.getBody();
     }
+//server가 어떤 식으로 데이터를 주는지 모를 때는 void로
+    //public UserResponse post(){
+    public void post(){
+        // http:// localhost: /api/server/user/{userID}/name/{userName}
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:8989")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100,"steve") //{}의 값들 순서대로 작성해줌
+                .toUri();
+        System.out.println(uri);
+
+        //http body -> object - > object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        //응답받을 형식
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+        //String형식으로 찍어줌. 형태 모를 때 사용가능(안맞아서 안찍히는 부분 알아낼 수 있음)
+        
+       // return response.getBody();
+    }
+    public UserResponse exchange(){
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:8989")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100,"steve") //{}의 값들 순서대로 작성해줌
+                .toUri();
+        System.out.println(uri);
+
+        //http body -> object - > object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RequestEntity<UserRequest> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization","abcd")
+                .header("custom-header", "ffffff")
+                .body(req);
+
+        //해더 실어서 보내기기
+       RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.exchange(requestEntity,UserResponse.class);
+        return response.getBody();
+
+    }
+
+    public Req<UserResponse> genericExchange(){
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:8989")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100,"steve") //{}의 값들 순서대로 작성해줌
+                .toUri();
+        System.out.println(uri);
+
+        //http body -> object - > object mapper -> json -> rest template -> http body json
+
+        UserRequest userRequest = new UserRequest();
+        userRequest.setName("steve");
+        userRequest.setAge(10);
+
+        Req<UserRequest> req = new Req<UserRequest>();
+        req.setHeader(
+            new Req.Header()
+
+        );
+        req.setResBody(
+            userRequest
+
+        );
+
+
+
+        RequestEntity<Req<UserRequest>> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("x-authorization","abcd")
+                .header("custom-header", "ffffff")
+                .body(req);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        ResponseEntity<Req<UserResponse>> response =
+                restTemplate.exchange(requestEntity,  new ParameterizedTypeReference<Req<UserResponse>>(){});
+        //new ParameterizedTypeReference<Req<UserResponse>>(){}; //Req<UserResponse>.class사용하고 싶을 때.
+        //제네릭 타입을 이미 선언해주었기 때문에 생략 가능
+
+        return response.getBody();
+        //첫번째 getBody()는 ResponseEntity의 getBody
+        //두번째 getBody()는 Req에 지정한 resBody
+
+    }
+
 }
--- Communication/server/src/main/java/com/example/server/controller/ServerApiController.java
+++ Communication/server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -1,13 +1,14 @@
 package com.example.server.controller;
 
+import com.example.server.dto.Req;
 import com.example.server.dto.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/server")
+@Slf4j
 public class ServerApiController {
 
     @GetMapping("/hello")
@@ -17,5 +18,30 @@ public class ServerApiController {
         user.setAge(age);
 
         return user;
+    }
+
+    @PostMapping("/user/{userId}/name/{userName}")
+    public Req<User> post(
+                     //HttpEntity<String> entity, //클라이언트가 보낸 값을 string으로 찍어낼 수 있음
+                     @RequestBody Req<User> user,
+                     @PathVariable int userId,
+                     @PathVariable String userName,
+                     @RequestHeader("x-authorization") String authorization,
+                     @RequestHeader("custom-header")String customHeader
+    ){
+        //log.info("req : {}", entity.getResBody());
+        log.info("userId: {}, userName: {}",userId, userName);
+        log.info("authorization : {}, custom :{}", authorization, customHeader); //header
+        log.info("client req : {}", user);
+
+        Req<User> response = new Req<>();
+        response.setHeader(
+                new Req.Header()
+        );
+        response.setResBody(
+                user.getResBody() //에코
+        );
+
+        return response;
     }
 }
